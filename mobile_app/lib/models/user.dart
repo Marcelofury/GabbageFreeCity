6ea5b40c78c870debb2a2bb742fdc84f74f4184c
Marcelofury <marcelofury13579@gmail.com>
2026-01-12 15:42:14 +0300

    added user dart file

---
+++ mobile_app/lib/models/user.dart
@@ -0,0 +1,50 @@
+/**
+ * User Model
+ */
+
+class User {
+  final String id;
+  final String phoneNumber;
+  final String fullName;
+  final String userType; // 'resident' or 'collector'
+  final String? email;
+  final String? area;
+  final bool isActive;
+
+  User({
+    required this.id,
+    required this.phoneNumber,
+    required this.fullName,
+    required this.userType,
+    this.email,
+    this.area,
+    this.isActive = true,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      phoneNumber: json['phone_number'],
+      fullName: json['full_name'],
+      userType: json['user_type'],
+      email: json['email'],
+      area: json['area'],
+      isActive: json['is_active'] ?? true,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'phone_number': phoneNumber,
+      'full_name': fullName,
+      'user_type': userType,
+      'email': email,
+      'area': area,
+      'is_active': isActive,
+    };
+  }
+
+  bool get isResident => userType == 'resident';
+  bool get isCollector => userType == 'collector';
+}
