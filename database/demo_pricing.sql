33aeec438bcd7b107ba4eff6aea7f88fb59f0338
Marcelofury <marcelofury13579@gmail.com>
2026-06-08 21:52:55 +0300

    updated subscription fee

--- database/demo_pricing.sql
+++ database/demo_pricing.sql
@@ -1,8 +1,8 @@
 -- Demo pricing update
 UPDATE subscription_plans
-SET monthly_price_ugx = 200, prepay_price_ugx = 600
+SET monthly_price_ugx = 1000, prepay_price_ugx = 3000
 WHERE weekly_collections = 1 AND monthly_collections = 4;
 
 UPDATE subscription_plans
-SET monthly_price_ugx = 400, prepay_price_ugx = 1200
+SET monthly_price_ugx = 2000, prepay_price_ugx = 6000
 WHERE weekly_collections = 2 AND monthly_collections = 8;
